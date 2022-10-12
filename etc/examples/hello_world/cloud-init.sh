3b1709059fe13a9959e396ee374c8d260fa7bd20
jzanetti <zsjzyhzp@gmail.com>
2022-10-13 10:07:25 +1300

    remove examples

--- etc/examples/hello_world/cloud-init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo aws s3 cp s3://xxxx-shiny-app/r/hello_world/ /srv/shiny-server/app/hello_world --recursive
-sudo cp -rf /srv/shiny-server/app/hello_world/shiny-server.conf /etc/shiny-server
-# sudo systemctl restart shiny-server
